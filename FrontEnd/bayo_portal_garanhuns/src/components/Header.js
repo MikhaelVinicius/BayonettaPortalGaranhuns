69869ac5ff5cbb9c8ec3a43eefd326c7b5193dd5
MikhaelVinicius <mickaelsoel@gmail.com>
2023-04-26 21:05:45 -0300

    Titulo adicionado

--- FrontEnd/bayo_portal_garanhuns/src/components/Header.js
+++ FrontEnd/bayo_portal_garanhuns/src/components/Header.js
@@ -1,16 +1,17 @@
+
+
+
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './Header.css';
 import brasao from '../assents/brasao.png';
-
-
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg" className="navbar">
       <Container>
         <Navbar.Brand href="#" className="navbar-brand">
-        <img src={brasao} alt="Brasão de Garanhuns" className="brand-logo" />
-          Portal de Turismo de Garanhuns
+          <img src={brasao} alt="Brasão de Garanhuns" className="brand-logo" />
+          <h1 className="brand-title">Portal do turismo de Garanhuns</h1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
