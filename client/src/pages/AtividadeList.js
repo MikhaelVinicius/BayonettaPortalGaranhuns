22670d9637fdc7d7c03d691071d5513346ca40ca
MikhaelVinicius <mickaelsoel@gmail.com>
2023-05-04 15:09:39 -0300

    Rotas foram direcionadas em app.js ao inves de homepage

--- client/src/pages/AtividadeList.js
+++ client/src/pages/AtividadeList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './AtividadeList.css';
+import { Link } from 'react-router-dom';
 
 function AtividadeList() {
   const [atividades, setAtividades] = useState([]);
@@ -33,7 +34,7 @@ function AtividadeList() {
             <h2>{atividade.nome}</h2>
             <p>{limitarDescricao(atividade.descricao)}</p>
             <p>{atividade.localizacao}</p>
-            <a href="#" className="cta-btn">Ver mais</a>
+            <Link to={`/atividades/${atividade.id}`} className="cta-btn">Ver mais</Link>
           </div>
         </div>
       ))}
@@ -42,5 +43,3 @@ function AtividadeList() {
 }
 
 export default AtividadeList;
-
-
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -3,9 +3,8 @@ import { Link } from 'react-router-dom';
 import './Homepage.css'; 
 import App from '../App';
 import PontoTuristicoList from './PontoTuristicoList';
-import AtividadeList from './AtividadeList';
-import HotelList from './HotelList';
-import RestauranteList from './RestauranteList';
+
+
 
 function Homepage() {
   const pontoTuristicoListRef = useRef(null);
@@ -31,46 +30,31 @@ function Homepage() {
 
   return (
     <div className="homepage">
-      <header>
-        <nav>
-          <Link to="/" className="logo">Portal de Turismo</Link>
-          <div className="search-box">
-            <input type="text" placeholder="Pesquisar..." />
-            <button type="submit">Buscar</button>
-          </div>
-          <ul className="main-menu">
-            <li><Link to="/pontos_turisticos"  onClick={scrollToPontoTuristicoList}>Pontos Turísticos</Link></li>
-            <li><Link to="/hospedagens" onClick={scrollToHotelList}>Hospedagens</Link></li>
-            <li><Link to="/restaurantes" onClick={scrollToRestauranteList}>Restaurantes</Link></li>
-            <li><Link to="/atividades" onClick={scrollToAtividadeList}>Atividades</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <h1>Bem-vindo ao Portal de Turismo</h1>
-        <p>Encontre os melhores destinos, hospedagens, restaurantes e atividades turísticas.</p>
-        <button onClick={scrollToPontoTuristicoList} className="cta-btn">Explorar</button>
-        
-      </main>
-      
-
-      <div ref={pontoTuristicoListRef}>
+    <header>
+      <nav>
+        <Link to="/" className="logo">Portal de Turismo</Link>
+        <div className="search-box">
+          <input type="text" placeholder="Pesquisar..." />
+          <button type="submit">Buscar</button>
+        </div>
+        <ul className="main-menu">
+          <li><Link to="/pontos_turisticos">Pontos Turísticos</Link></li>
+          <li><Link to="/hospedagens">Hospedagens</Link></li>
+          <li><Link to="/restaurantes">Restaurantes</Link></li>
+          <li><Link to="/atividades">Atividades</Link></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>Bem-vindo ao Portal de Turismo</h1>
+      <p>Encontre os melhores destinos, hospedagens, restaurantes e atividades turísticas.</p>
+      <button onClick={scrollToPontoTuristicoList} className="cta-btn">Explorar</button>
+    </main>
+    
+    <div ref={pontoTuristicoListRef}>
         <PontoTuristicoList />
       </div>
-      
-      <div ref={atividadeListRef}>
-        <AtividadeList />
-      </div>
-
-      <div ref={hotelListRef}>
-        <HotelList />
-      </div>
-
-      <div ref={restauranteListRef}>
-        <RestauranteList />
-      </div>
-
-    </div>
+  </div>
   );
 }
 
